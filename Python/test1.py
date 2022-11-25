5f69e9812df9103e216b584aace3f01d70b77659
SnakeorSnack <118553876+SnakeorSnack@users.noreply.github.com>
2022-11-25 22:12:54 +0100

    update

--- Python/test1.py
+++ Python/test1.py
@@ -2,9 +2,14 @@ from collections import Counter
 
 massive ='маша маша леша кошка суп картошка блоша пирог чипсики чипсики маша леша чипсики шерсть нитки иголки чипсики чипсики чипсики'
 
+
+# split() без аргументов делит по пробельным символам, в том числе и по переносам строк
 massive_split = massive.split()
+# один проход с подсчетом в словаре - примерно O(n)
 x = Counter(massive_split)
+# нахождение максимума - О(n)
 max_encounters = max(x.values())
+# нахождение минимального из максимально частых слов O(n*m) (m - количество самых частых слов)
 most_common_word = min(word for word, count in x.items() if count == max_encounters)
 print(most_common_word)
 
@@ -12,16 +17,3 @@ print(most_common_word)
 
 
 
-
-
-
-
-
-
-
-
-# (myLine = text.split()  # split() без аргументов делит по пробельным символам, в том числе и по переносам строк
-# d = Counter(myLine)  # один проход с подсчетом в словаре - примерно O(n)
-# max_encounters = max(d.values())  # нахождение максимума - О(n)
-# most_common_word = min(word for word, count in d.items() if count == max_encounters)  # нахождение минимального из максимально частых слов O(n*m) (m - количество самых частых слов)
-# print(most_common_word) 
